a3b0400f9419b8ea93f2fb34c25ab8f7c546d1bb
David Vaz Cortesão <davidvazc@gmail.com>
2019-05-27 11:47:13 +0100

    a funcionar

--- RgbImage.cpp
+++ RgbImage.cpp
@@ -21,36 +21,25 @@
 #include "RgbImage.h"
 
 #ifndef RGBIMAGE_DONT_USE_OPENGL
-#include <stdio.h> // printf
-#include <stdlib.h>
-#include <math.h>
-#include <MacTypes.h>
-#include <time.h>
-#include <OpenGL/gl.h>
-#include <OpenGL/glu.h>
-#include <GLUT/glut.h>
-
+#include <GL/gl.h>
 #endif
 
-RgbImage::RgbImage(int numRows, int numCols)
+RgbImage::RgbImage( int numRows, int numCols )
 {
 	NumRows = numRows;
 	NumCols = numCols;
-	ImagePtr = new unsigned char[NumRows * GetNumBytesPerRow()];
-	if (!ImagePtr)
-	{
+	ImagePtr = new unsigned char[NumRows*GetNumBytesPerRow()];
+	if ( !ImagePtr ) {
 		fprintf(stderr, "Unable to allocate memory for %ld x %ld bitmap.\n",
 				NumRows, NumCols);
 		Reset();
 		ErrorCode = MemoryError;
 	}
 	// Zero out the image
-	unsigned char *c = ImagePtr;
+	unsigned char* c = ImagePtr;
 	int rowLen = GetNumBytesPerRow();
-	for (int i = 0; i < NumRows; i++)
-	{
-		for (int j = 0; j < rowLen; j++)
-		{
+	for ( int i=0; i<NumRows; i++ ) {
+		for ( int j=0; j<rowLen; j++ ) {
 			*(c++) = 0;
 		}
 	}
@@ -64,91 +53,83 @@ RgbImage::RgbImage(int numRows, int numCols)
  *  Author: Sam Buss December 2001.
  **********************************************************************/
 
-bool RgbImage::LoadBmpFile(const char *filename)
+bool RgbImage::LoadBmpFile( const char* filename )
 {
 	Reset();
-	FILE *infile = fopen(filename, "rb"); // Open for reading binary data
-	if (!infile)
-	{
+	FILE* infile = fopen( filename, "rb" );		// Open for reading binary data
+	if ( !infile ) {
 		fprintf(stderr, "Unable to open file: %s\n", filename);
 		ErrorCode = OpenError;
 		return false;
 	}
 
 	bool fileFormatOK = false;
-	int bChar = fgetc(infile);
-	int mChar = fgetc(infile);
-	if (bChar == 'B' && mChar == 'M')
-	{										  // If starts with "BM" for "BitMap"
-		skipChars(infile, 4 + 2 + 2 + 4 + 4); // Skip 4 fields we don't care about
-		NumCols = readLong(infile);
-		NumRows = readLong(infile);
-		skipChars(infile, 2); // Skip one field
-		int bitsPerPixel = readShort(infile);
-		skipChars(infile, 4 + 4 + 4 + 4 + 4 + 4); // Skip 6 more fields
+	int bChar = fgetc( infile );
+	int mChar = fgetc( infile );
+	if ( bChar=='B' && mChar=='M' ) {			// If starts with "BM" for "BitMap"
+		skipChars( infile, 4+2+2+4+4 );			// Skip 4 fields we don't care about
+		NumCols = readLong( infile );
+		NumRows = readLong( infile );
+		skipChars( infile, 2 );					// Skip one field
+		int bitsPerPixel = readShort( infile );
+		skipChars( infile, 4+4+4+4+4+4 );		// Skip 6 more fields
 
-		if (NumCols > 0 && NumCols <= 100000 && NumRows > 0 && NumRows <= 100000 && bitsPerPixel == 24 && !feof(infile))
-		{
+		if ( NumCols>0 && NumCols<=100000 && NumRows>0 && NumRows<=100000
+			&& bitsPerPixel==24 && !feof(infile) ) {
 			fileFormatOK = true;
 		}
 	}
-	if (!fileFormatOK)
-	{
+	if ( !fileFormatOK ) {
 		Reset();
 		ErrorCode = FileFormatError;
 		fprintf(stderr, "Not a valid 24-bit bitmap file: %s.\n", filename);
-		fclose(infile);
+		fclose ( infile );
 		return false;
 	}
 
 	// Allocate memory
-	ImagePtr = new unsigned char[NumRows * GetNumBytesPerRow()];
-	if (!ImagePtr)
-	{
+	ImagePtr = new unsigned char[NumRows*GetNumBytesPerRow()];
+	if ( !ImagePtr ) {
 		fprintf(stderr, "Unable to allocate memory for %ld x %ld bitmap: %s.\n",
 				NumRows, NumCols, filename);
 		Reset();
 		ErrorCode = MemoryError;
-		fclose(infile);
+		fclose ( infile );
 		return false;
 	}
 
-	unsigned char *cPtr = ImagePtr;
-	for (int i = 0; i < NumRows; i++)
-	{
+	unsigned char* cPtr = ImagePtr;
+	for ( int i=0; i<NumRows; i++ ) {
 		int j;
-		for (j = 0; j < NumCols; j++)
-		{
-			*(cPtr + 2) = fgetc(infile); // Blue color value
-			*(cPtr + 1) = fgetc(infile); // Green color value
-			*cPtr = fgetc(infile);		 // Red color value
+		for ( j=0; j<NumCols; j++ ) {
+			*(cPtr+2) = fgetc( infile );	// Blue color value
+			*(cPtr+1) = fgetc( infile );	// Green color value
+			*cPtr = fgetc( infile );		// Red color value
 			cPtr += 3;
 		}
-		int k = 3 * NumCols; // Num bytes already read
-		for (; k < GetNumBytesPerRow(); k++)
-		{
-			fgetc(infile); // Read and ignore padding;
+		int k=3*NumCols;					// Num bytes already read
+		for ( ; k<GetNumBytesPerRow(); k++ ) {
+			fgetc( infile );				// Read and ignore padding;
 			*(cPtr++) = 0;
 		}
 	}
-	if (feof(infile))
-	{
-		fprintf(stderr, "Premature end of file: %s.\n", filename);
+	if ( feof( infile ) ) {
+		fprintf( stderr, "Premature end of file: %s.\n", filename );
 		Reset();
 		ErrorCode = ReadError;
-		fclose(infile);
+		fclose ( infile );
 		return false;
 	}
-	fclose(infile); // Close the file
+	fclose( infile );	// Close the file
 	return true;
 }
 
-short RgbImage::readShort(FILE *infile)
+short RgbImage::readShort( FILE* infile )
 {
 	// read a 16 bit integer
 	unsigned char lowByte, hiByte;
-	lowByte = fgetc(infile); // Read the low order byte (little endian form)
-	hiByte = fgetc(infile);  // Read the high order byte
+	lowByte = fgetc(infile);			// Read the low order byte (little endian form)
+	hiByte = fgetc(infile);			// Read the high order byte
 
 	// Pack together
 	short ret = hiByte;
@@ -157,11 +138,11 @@ short RgbImage::readShort(FILE *infile)
 	return ret;
 }
 
-long RgbImage::readLong(FILE *infile)
+long RgbImage::readLong( FILE* infile )
 {
 	// Read in 32 bit integer
 	unsigned char byte0, byte1, byte2, byte3;
-	byte0 = fgetc(infile); // Read bytes, low order to high order
+	byte0 = fgetc(infile);			// Read bytes, low order to high order
 	byte1 = fgetc(infile);
 	byte2 = fgetc(infile);
 	byte3 = fgetc(infile);
@@ -177,11 +158,10 @@ long RgbImage::readLong(FILE *infile)
 	return ret;
 }
 
-void RgbImage::skipChars(FILE *infile, int numChars)
+void RgbImage::skipChars( FILE* infile, int numChars )
 {
-	for (int i = 0; i < numChars; i++)
-	{
-		fgetc(infile);
+	for ( int i=0; i<numChars; i++ ) {
+		fgetc( infile );
 	}
 }
 
@@ -193,121 +173,116 @@ void RgbImage::skipChars(FILE *infile, int numChars)
  *  Author: Sam Buss, January 2003.
  **********************************************************************/
 
-bool RgbImage::WriteBmpFile(const char *filename)
+bool RgbImage::WriteBmpFile( const char* filename )
 {
-	FILE *outfile = fopen(filename, "wb"); // Open for reading binary data
-	if (!outfile)
-	{
+	FILE* outfile = fopen( filename, "wb" );		// Open for reading binary data
+	if ( !outfile ) {
 		fprintf(stderr, "Unable to open file: %s\n", filename);
 		ErrorCode = OpenError;
 		return false;
 	}
 
-	fputc('B', outfile);
-	fputc('M', outfile);
+	fputc('B',outfile);
+	fputc('M',outfile);
 	int rowLen = GetNumBytesPerRow();
-	writeLong(40 + 14 + NumRows * rowLen, outfile); // Length of file
-	writeShort(0, outfile);							// Reserved for future use
-	writeShort(0, outfile);
-	writeLong(40 + 14, outfile); // Offset to pixel data
-	writeLong(40, outfile);		 // header length
-	writeLong(NumCols, outfile); // width in pixels
-	writeLong(NumRows, outfile); // height in pixels (pos for bottom up)
-	writeShort(1, outfile);		 // number of planes
-	writeShort(24, outfile);	 // bits per pixel
-	writeLong(0, outfile);		 // no compression
-	writeLong(0, outfile);		 // not used if no compression
-	writeLong(0, outfile);		 // Pixels per meter
-	writeLong(0, outfile);		 // Pixels per meter
-	writeLong(0, outfile);		 // unused for 24 bits/pixel
-	writeLong(0, outfile);		 // unused for 24 bits/pixel
+	writeLong( 40+14+NumRows*rowLen, outfile );	// Length of file
+	writeShort( 0, outfile );					// Reserved for future use
+	writeShort( 0, outfile );
+	writeLong( 40+14, outfile );				// Offset to pixel data
+	writeLong( 40, outfile );					// header length
+	writeLong( NumCols, outfile );				// width in pixels
+	writeLong( NumRows, outfile );				// height in pixels (pos for bottom up)
+	writeShort( 1, outfile );		// number of planes
+	writeShort( 24, outfile );		// bits per pixel
+	writeLong( 0, outfile );		// no compression
+	writeLong( 0, outfile );		// not used if no compression
+	writeLong( 0, outfile );		// Pixels per meter
+	writeLong( 0, outfile );		// Pixels per meter
+	writeLong( 0, outfile );		// unused for 24 bits/pixel
+	writeLong( 0, outfile );		// unused for 24 bits/pixel
 
 	// Now write out the pixel data:
-	unsigned char *cPtr = ImagePtr;
-	for (int i = 0; i < NumRows; i++)
-	{
+	unsigned char* cPtr = ImagePtr;
+	for ( int i=0; i<NumRows; i++ ) {
 		// Write out i-th row's data
 		int j;
-		for (j = 0; j < NumCols; j++)
-		{
-			fputc(*(cPtr + 2), outfile); // Blue color value
-			fputc(*(cPtr + 1), outfile); // Blue color value
-			fputc(*(cPtr + 0), outfile); // Blue color value
-			cPtr += 3;
+		for ( j=0; j<NumCols; j++ ) {
+			fputc( *(cPtr+2), outfile);		// Blue color value
+			fputc( *(cPtr+1), outfile);		// Blue color value
+			fputc( *(cPtr+0), outfile);		// Blue color value
+			cPtr+=3;
 		}
 		// Pad row to word boundary
-		int k = 3 * NumCols; // Num bytes already read
-		for (; k < GetNumBytesPerRow(); k++)
-		{
-			fputc(0, outfile); // Read and ignore padding;
+		int k=3*NumCols;					// Num bytes already read
+		for ( ; k<GetNumBytesPerRow(); k++ ) {
+			fputc( 0, outfile );				// Read and ignore padding;
 			cPtr++;
 		}
 	}
 
-	fclose(outfile); // Close the file
+	fclose( outfile );	// Close the file
 	return true;
 }
 
-void RgbImage::writeLong(long data, FILE *outfile)
+void RgbImage::writeLong( long data, FILE* outfile )
 {
 	// Read in 32 bit integer
 	unsigned char byte0, byte1, byte2, byte3;
-	byte0 = (unsigned char)(data & 0x000000ff); // Write bytes, low order to high order
-	byte1 = (unsigned char)((data >> 8) & 0x000000ff);
-	byte2 = (unsigned char)((data >> 16) & 0x000000ff);
-	byte3 = (unsigned char)((data >> 24) & 0x000000ff);
+	byte0 = (unsigned char)(data&0x000000ff);		// Write bytes, low order to high order
+	byte1 = (unsigned char)((data>>8)&0x000000ff);
+	byte2 = (unsigned char)((data>>16)&0x000000ff);
+	byte3 = (unsigned char)((data>>24)&0x000000ff);
 
-	fputc(byte0, outfile);
-	fputc(byte1, outfile);
-	fputc(byte2, outfile);
-	fputc(byte3, outfile);
+	fputc( byte0, outfile );
+	fputc( byte1, outfile );
+	fputc( byte2, outfile );
+	fputc( byte3, outfile );
 }
 
-void RgbImage::writeShort(short data, FILE *outfile)
+void RgbImage::writeShort( short data, FILE* outfile )
 {
 	// Read in 32 bit integer
 	unsigned char byte0, byte1;
-	byte0 = data & 0x000000ff; // Write bytes, low order to high order
-	byte1 = (data >> 8) & 0x000000ff;
+	byte0 = data&0x000000ff;		// Write bytes, low order to high order
+	byte1 = (data>>8)&0x000000ff;
 
-	fputc(byte0, outfile);
-	fputc(byte1, outfile);
+	fputc( byte0, outfile );
+	fputc( byte1, outfile );
 }
+
 
 /*********************************************************************
  * SetRgbPixel routines allow changing the contents of the RgbImage. *
  *********************************************************************/
 
-void RgbImage::SetRgbPixelf(long row, long col, double red, double green, double blue)
+void RgbImage::SetRgbPixelf( long row, long col, double red, double green, double blue )
 {
-	SetRgbPixelc(row, col, doubleToUnsignedChar(red),
-				 doubleToUnsignedChar(green),
-				 doubleToUnsignedChar(blue));
+	SetRgbPixelc( row, col, doubleToUnsignedChar(red),
+							doubleToUnsignedChar(green),
+							doubleToUnsignedChar(blue) );
 }
 
-void RgbImage::SetRgbPixelc(long row, long col,
-							unsigned char red, unsigned char green, unsigned char blue)
+void RgbImage::SetRgbPixelc( long row, long col,
+				   unsigned char red, unsigned char green, unsigned char blue )
 {
-	assert(row < NumRows && col < NumCols);
-	unsigned char *thePixel = GetRgbPixel(row, col);
+	assert ( row<NumRows && col<NumCols );
+	unsigned char* thePixel = GetRgbPixel( row, col );
 	*(thePixel++) = red;
 	*(thePixel++) = green;
 	*(thePixel) = blue;
 }
 
-unsigned char RgbImage::doubleToUnsignedChar(double x)
+
+unsigned char RgbImage::doubleToUnsignedChar( double x )
 {
-	if (x >= 1.0)
-	{
+	if ( x>=1.0 ) {
 		return (unsigned char)255;
 	}
-	else if (x <= 0.0)
-	{
+	else if ( x<=0.0 ) {
 		return (unsigned char)0;
 	}
-	else
-	{
-		return (unsigned char)(x * 255.0); // Rounds down
+	else {
+		return (unsigned char)(x*255.0);		// Rounds down
 	}
 }
 // Bitmap file format  (24 bit/pixel form)		BITMAPFILEHEADER
@@ -332,22 +307,21 @@ unsigned char RgbImage::doubleToUnsignedChar(double x)
 // Pixel data: 3 bytes per pixel: RGB values (in reverse order).
 //	Rows padded to multiples of four.
 
+
 #ifndef RGBIMAGE_DONT_USE_OPENGL
 
-bool RgbImage::LoadFromOpenglBuffer() // Load the bitmap from the current OpenGL buffer
+bool RgbImage::LoadFromOpenglBuffer()					// Load the bitmap from the current OpenGL buffer
 {
 	int viewportData[4];
-	glGetIntegerv(GL_VIEWPORT, viewportData);
-	int &vWidth = viewportData[2];
-	int &vHeight = viewportData[3];
+	glGetIntegerv( GL_VIEWPORT, viewportData );
+	int& vWidth = viewportData[2];
+	int& vHeight = viewportData[3];
 
-	if (ImagePtr == 0)
-	{ // If no memory allocated
+	if ( ImagePtr==0 ) { // If no memory allocated
 		NumRows = vHeight;
 		NumCols = vWidth;
-		ImagePtr = new unsigned char[NumRows * GetNumBytesPerRow()];
-		if (!ImagePtr)
-		{
+		ImagePtr = new unsigned char[NumRows*GetNumBytesPerRow()];
+		if ( !ImagePtr ) {
 			fprintf(stderr, "Unable to allocate memory for %ld x %ld buffer.\n",
 					NumRows, NumCols);
 			Reset();
@@ -355,24 +329,22 @@ bool RgbImage::LoadFromOpenglBuffer() // Load the bitmap from the current OpenGL
 			return false;
 		}
 	}
-	assert(vWidth >= NumCols && vHeight >= NumRows);
+	assert ( vWidth>=NumCols && vHeight>=NumRows );
 	int oldGlRowLen;
-	if (vWidth >= NumCols)
-	{
-		glGetIntegerv(GL_UNPACK_ROW_LENGTH, &oldGlRowLen);
-		glPixelStorei(GL_UNPACK_ROW_LENGTH, NumCols);
+	if ( vWidth>=NumCols ) {
+		glGetIntegerv( GL_UNPACK_ROW_LENGTH, &oldGlRowLen );
+		glPixelStorei( GL_UNPACK_ROW_LENGTH, NumCols );
 	}
 	glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
 
 	// Get the frame buffer data.
-	glReadPixels(0, 0, NumCols, NumRows, GL_RGB, GL_UNSIGNED_BYTE, ImagePtr);
+	glReadPixels( 0, 0, NumCols, NumRows, GL_RGB, GL_UNSIGNED_BYTE, ImagePtr);
 
 	// Restore the row length in glPixelStorei  (really ought to restore alignment too).
-	if (vWidth >= NumCols)
-	{
-		glPixelStorei(GL_UNPACK_ROW_LENGTH, oldGlRowLen);
+	if ( vWidth>=NumCols ) {
+		glPixelStorei( GL_UNPACK_ROW_LENGTH, oldGlRowLen );
 	}
 	return true;
 }
 
-#endif // RGBIMAGE_DONT_USE_OPENGL
+#endif   // RGBIMAGE_DONT_USE_OPENGL
